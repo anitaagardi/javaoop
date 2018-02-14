aaaaa2a63f659566c7082385b6917ac26df444df
anitaagardi <anitaagardi@gmail.com>
2018-02-14 15:39:51 +0100

    inital commit

---
+++ HelloWorld/src/hello/HelloWorld.java
@@ -0,0 +1,19 @@
+package hello;
+
+public class HelloWorld {
+	/**
+	 * 
+	 * @param args argumentum
+	 */
+	public static void main(String[] args) {
+		//Komment
+		//Komment2
+		/*
+		 * Kommentek
+		 * XYZ
+		 */
+		System.out.println("Hello Anita!");
+
+	}
+
+}
